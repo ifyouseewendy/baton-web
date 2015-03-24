50cb583c6b20edb511d417dccf37b0b8b6301b40
wendi <ifyouseewendy@gmail.com>
2015-03-24 16:23:02 +0800

    Make data#index render daily stat table

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,5 +1,14 @@
 class DataController < ApplicationController
   def index
+    platform = Platform.where(name: 'xiaojin').first
+    project  = platform.projects.where(name: 'alpha').first
+
+    @daily_stats = {}
+    project.hourly_stats.asc(:date).asc(:hour).each do |hs|
+      @daily_stats[hs.date] ||= {order_count: 0, share_count: 0}
+      @daily_stats[hs.date][:order_count] += hs.order_count
+      @daily_stats[hs.date][:share_count] += hs.share_count
+    end
   end
 
   def get_hourly
