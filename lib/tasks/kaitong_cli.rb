bb37841a4ce04f605aa593da622fe3acaf1a1f9b
wendi <ifyouseewendy@gmail.com>
2015-04-11 12:52:13 +0800

    Rename to xiaojin thor, and extract kaitong_cli with load_rails

--- lib/tasks/kaitong_cli.rb
+++ lib/tasks/kaitong_cli.rb
@@ -1,61 +1,10 @@
 require 'thor'
-require 'csv'
 
 class KaitongCli < Thor
-  ENV['RAILS_ENV'] ||= 'development'
-  require File.expand_path('config/environment.rb')
-
-  desc "hello NAME", "say hello to NAME"
-  def hello(name)
-    puts "Hello #{name}"
-  end
-
-  MAX_CODE_CAPACITY = 200
-
-  desc 'xiaojin', '将资产单元编号映射成产品代码'
-  long_desc <<-LONGDESC
-    Examples:
-
-      ruby lib/tasks/kaitong_cli.rb xiaojin --code=K00001 --column=2
-        --from=/Users/wendi/Workspace/kaitong/ftp-monitor/test/tasks/resources/客户销售明细表.csv
-  LONGDESC
-  option :from,   required: true
-  option :column, type: :numeric, required: true
-  option :code,   required: true
-  def xiaojin
-    raise "Invalid <from> file position: #{options[:from]}" unless File.exist?(options[:from])
-
-    column = options[:column]
-    output = "#{options[:from]}.out.csv"
-    code = ProductCode.new(options[:code])
-    File.open(output, 'w') do |of|
-      pos = 0
-      CSV.foreach(options[:from]) do |row|
-        pos += 1
-
-        next if row[0].blank?
-
-        of.puts (row[0..(column-1)] + [code.to_s] + row[(column+1)..-1]).join(',')
-        record_relation(row[column], code)
-
-        if pos == MAX_CODE_CAPACITY
-          pos = 0
-          code.next!
-        end
-      end
-    end
-
-    puts "--> Generate file: #{output}"
-    puts "    Code range: #{options[:code]} - #{code}"
-  end
 
   private
 
-    def record_relation(stat, code)
-      prd = Product.find_or_create_by(code: code.to_s)
-      prd.property_serial << stat
-      prd.save!
+    def load_rails
+      require File.expand_path('config/environment.rb')
     end
 end
-
-KaitongCli.start(ARGV)
---
+++ lib/tasks/xiaojin.rb
@@ -0,0 +1,60 @@
+require_relative 'kaitong_cli'
+require 'csv'
+
+class KaitongCli < Thor
+  desc "hello NAME", "say hello to NAME"
+  def hello(name)
+    puts "Hello #{name}"
+  end
+
+  MAX_CODE_CAPACITY = 200
+
+  desc 'convert', '将资产单元编号映射成产品代码'
+  long_desc <<-LONGDESC
+    Examples:
+
+      ruby lib/tasks/xiaojin.rb convert --code=K00001 --column=2
+        --from=/Users/wendi/Workspace/kaitong/ftp-monitor/test/tasks/resources/客户销售明细表.csv
+  LONGDESC
+  option :from,   required: true
+  option :column, type: :numeric, required: true
+  option :code,   required: true
+  def convert
+    raise "Invalid <from> file position: #{options[:from]}" unless File.exist?(options[:from])
+
+    load_rails
+
+    column = options[:column]
+    output = "#{options[:from]}.out.csv"
+    code = ProductCode.new(options[:code])
+    File.open(output, 'w') do |of|
+      pos = 0
+      CSV.foreach(options[:from]) do |row|
+        pos += 1
+
+        next if row[0].blank?
+
+        of.puts (row[0..(column-1)] + [code.to_s] + row[(column+1)..-1]).join(',')
+        record_relation(row[column], code)
+
+        if pos == MAX_CODE_CAPACITY
+          pos = 0
+          code.next!
+        end
+      end
+    end
+
+    puts "--> Generate file: #{output}"
+    puts "    Code range: #{options[:code]} - #{code}"
+  end
+
+  private
+
+    def record_relation(stat, code)
+      prd = Product.find_or_create_by(code: code.to_s)
+      prd.property_serial << stat
+      prd.save!
+    end
+end
+
+KaitongCli.start(ARGV)
