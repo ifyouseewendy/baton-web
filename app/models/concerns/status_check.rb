16c6fae6c7c132ab66e96d7bf62e2caea235dda5
wendi <ifyouseewendy@gmail.com>
2015-05-18 15:02:42 +0800

    Add done! in StatusCheck

--- app/models/concerns/status_check.rb
+++ app/models/concerns/status_check.rb
@@ -1,18 +1,19 @@
+# Overwrites done? and done! predication which Mongoid::Enum provides
 module StatusCheck
-  # Customized done? method, overwriting the enum ones
   def done?
     self.status == :done ? true : check_status!
   end
 
-  def fuck
-    puts 'fuck'
+  def done!
+    self.update_attribute(:status, :done)
+    steps.map(&:done!)
   end
 
   private
 
     def check_status!
       return false unless steps.all?(&:done?)
-      self.done!
+      self.update_attribute(:status, :done)
     end
 
 end
--- test/models/status_check_test.rb
+++ test/models/status_check_test.rb
@@ -8,4 +8,15 @@ module StatusCheckTest
     assert  @subject.done?
     assert_equal :done, @subject.status
   end
+
+  def test_customized_done!
+    assert_equal :undone, @subject.status
+    step = @subject.steps.first
+    assert_equal :undone, step.status
+
+    @subject.done!
+
+    assert_equal :done, step.status
+    assert_equal :done, @subject.status
+  end
 end
