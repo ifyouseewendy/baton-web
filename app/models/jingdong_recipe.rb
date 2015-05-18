9334159dbb55cd8f655d23c9178b4776df5b046f
wendi <ifyouseewendy@gmail.com>
2015-05-18 19:23:28 +0800

    Fix JingdongRecipe build

--- app/models/jingdong_recipe.rb
+++ app/models/jingdong_recipe.rb
@@ -9,9 +9,9 @@ class JingdongRecipe
     project = self.projects.create(name: source['name'], description: source['description'])
     source['stages'].each do |stage_source|
       stage = project.stages.create(name: stage_source['name'], description: stage_source['description'])
-      stage_source['tasks'].each do |task_source|
+      stage_source['tasks'].try(:each) do |task_source|
         task = stage.tasks.create(name: task_source['name'], description: task_source['description'])
-        task_source['steps'].each do |step_source|
+        task_source['steps'].try(:each) do |step_source|
           task.steps.create(name: step_source['name'], description: step_source['description'])
         end
       end
