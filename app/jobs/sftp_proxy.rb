f8d709b7a316a6b0067b20614f34d7af0e9f71a1
wendi <ifyouseewendy@gmail.com>
2015-05-20 00:49:32 +0800

    Add SftpProxy#download_dir

--- app/jobs/sftp_proxy.rb
+++ app/jobs/sftp_proxy.rb
@@ -7,5 +7,18 @@ class SftpProxy
         yield sftp
       end
     end
+
+    def download
+    end
+
+    # Download from/a, from/b -> to/a, to/b. No nested dir support.
+    def download_dir(from, to)
+      start do |sftp|
+        sftp.dir.foreach(from) do |file|
+          next if file.name =~ /^\./
+          sftp.download! File.join(from,file.name), File.join(to,file.name)
+        end
+      end
+    end
   end
 end
