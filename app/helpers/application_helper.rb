3c711d48c9a7358fd38a8e06d3a6405d3edf8beb
wendi <ifyouseewendy@gmail.com>
2015-05-20 22:17:35 +0800

    Extract html_format into ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+
+  def html_format(result)
+    return nil if result.blank?
+
+    ret = {status: result[:status]}
+
+    if result[:status] == :succeed
+      data = result[:stat]
+      if result[:type] == :file_list
+        ret[:html] = \
+          content_tag(
+            :ul,
+            -> {
+              data.reduce('') do |str, ar|
+                str << \
+                  content_tag(:li) do
+                    link_to(ar[0], ar[1])
+                  end
+              end
+            }.call,
+            nil,
+            false
+          )
+      else
+      end
+    else
+      ret[:html] = content_tag(:p, result[:message])
+    end
+
+    ret
+  end
+
 end
