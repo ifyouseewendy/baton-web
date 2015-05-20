8dab0f429711c67bcee93c312c9c27d1147972d2
wendi <ifyouseewendy@gmail.com>
2015-05-20 21:56:12 +0800

    Show step result in html_format

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -20,4 +20,34 @@ class StepsController < ApplicationController
     def stage_params
       params.permit(:project_id, :stage_id, :task_id, :id, :stub)
     end
+
+    def html_format(result)
+      ret = {status: result[:status]}
+
+      if result[:status] == :succeed
+        data = result[:stat]
+        if result[:type] == :file_list
+          ret[:html] = \
+            view_context.content_tag(
+              :ul,
+              -> {
+                data.reduce('') do |str, ar|
+                  str << \
+                    view_context.content_tag(:li) do
+                      view_context.link_to(ar[0], ar[1])
+                    end
+                end
+              }.call,
+              nil,
+              false
+            )
+        else
+        end
+      else
+        ret[:html] = view_context.content_tag(:p, result[:message])
+      end
+
+      ret
+    end
+
 end
