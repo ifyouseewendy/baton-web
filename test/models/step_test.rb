c24f6b15abb92ad2df6f751c5313c6b25bf1abac
wendi <ifyouseewendy@gmail.com>
2015-05-28 20:58:10 +0800

    Add new step test

--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -11,4 +11,38 @@ class StepTest < ActiveSupport::TestCase
   def teardown
     @step.delete
   end
+
+  module ::TestJob
+    class Step007
+      def run(step, args); {status: :succeed}; end
+    end
+  end
+
+  def test_run
+    project = stub(id: 'test')
+    @step.stubs(:project).returns(project)
+    @step.stubs(:recipe).returns(:test)
+    @step.stubs(:job_id).returns('007')
+
+    @step.run( {name: "Jissbon"} )
+
+    assert_equal :succeed, @step.result[:status]
+    assert @step.done?
+  end
+
+  def test_add_file
+    project = stub(id: 'test')
+    @step.stubs(:project).returns(project)
+
+    filename = Tempfile.create('_').path
+
+    assert_difference ->{ @step.files.count }, 2 do
+      @step.add_file(filename, :test)
+      @step.add_file(filename, :test)
+    end
+
+    assert_difference -> { @step.files.count }, -1 do
+      @step.add_file(filename, :test, override: true)
+    end
+  end
 end
