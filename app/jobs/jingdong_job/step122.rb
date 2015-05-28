eca29c8116b0d3a63f2f5f0cbbf82731d7ab9ea0
wendi <ifyouseewendy@gmail.com>
2015-05-28 23:04:25 +0800

    Add step 122

--- app/jobs/jingdong_job/step122.rb
+++ app/jobs/jingdong_job/step122.rb
@@ -2,13 +2,42 @@ module JingdongJob
   class Step122
     # 检验文件内容与编码
     def run(step, args)
-      {
-        status: :failed,
-        message: 'sucked'
-      }
-      {
-        status: :succeed,
-      }
+      begin
+        @file = step.task.files.first.try(:file)
+        @pa = Pathname.new @file.try(:current_path)
+        check_existence!
+        check_ext_type!
+
+        # Make this operation in step 121
+        # auto_correct_name!
+
+        {
+          status: :succeed
+        }
+      rescue => e
+        {
+          status: :failed,
+          message: e.message
+        }
+      end
     end
+
+    private
+
+      def check_existence!
+        raise "文件不存在 - #{@pa}" unless @pa.exist?
+      end
+
+      def check_ext_type!
+        raise "文件类型不是 xls(x) - #{@pa.basename}" unless %w(.xls .xlsx).include?(@pa.extname)
+      end
+
+      def auto_correct_name!
+        name = ["kaitong_product_apply_#{Date.today.to_s.gsub('-','')}", @pa.extname].join
+        unless name == @pa.basename.to_s
+          @file.rename! name
+        end
+      end
+
   end
 end
