5c09ab9041a9bd3292b6b436aaa82a0f59710ebe
wendi <ifyouseewendy@gmail.com>
2015-05-28 16:12:50 +0800

    Fix step111 to rescue exception

--- app/jobs/jingdong_job/step111.rb
+++ app/jobs/jingdong_job/step111.rb
@@ -2,26 +2,33 @@ module JingdongJob
   class Step111
     # 检查广交所是否上传文件至 FTP
     def run(step, args)
-      fa = FileAgent.new(:guangjiaosuo)
-      fa.download(:dir, args)
+      begin
+        fa = FileAgent.new(:guangjiaosuo)
+        fa.download(:dir, args)
 
-      if fa.files.count == 0
-        {
-          status: 'failed',
-          message: "未检查到文件"
-        }
-      else
-        fa.files.each {|pa| step.add_file(pa) }
+        if fa.files.count == 0
+          {
+            status: :failed,
+            message: "未检查到文件"
+          }
+        else
+          fa.files.each {|pa| step.add_file(pa) }
 
-        names, links = fa.names, fa.links
-        {
-          status: :succeed,
-          type: :file_list,
-          query: {
-            date: args[:date]
-          },
-          stat: names.zip(links)
-        }
+          names, links = fa.names, fa.links
+          {
+            status: :succeed,
+            type: :file_list,
+            query: {
+              date: args[:date]
+            },
+            stat: names.zip(links)
+          }
+        end
+      rescue => e
+          {
+            status: :failed,
+            message: e.message
+          }
       end
     end
   end
