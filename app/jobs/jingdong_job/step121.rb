4f83727de8692985e07669481150aebb52b91e77
wendi <ifyouseewendy@gmail.com>
2015-05-28 16:14:39 +0800

    Add step121 run

--- app/jobs/jingdong_job/step121.rb
+++ app/jobs/jingdong_job/step121.rb
@@ -2,6 +2,27 @@ module JingdongJob
   class Step121
     # 上传《产品发布文件》
     def run(step, args)
+      begin
+        source            = Pathname(args[:file].tempfile)
+        original_filename = args[:file].original_filename
+        target_path       = Pathname(source.dirname).join(original_filename)
+        FileUtils.mv( source, target_path)
+        raise 'hello world'
+
+        step.add_file(target_path, override: true)
+
+        {
+          status: :succeed,
+          type: :file_list,
+          stat: step.file_names.zip(step.file_urls)
+        }
+      rescue => e
+        {
+          status: :failed,
+          message: e.message
+        }
+      end
     end
+
   end
 end
