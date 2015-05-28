da3ba5d9e375ec7ad2454d22bb32269e019db73e
wendi <ifyouseewendy@gmail.com>
2015-05-28 19:19:09 +0800

    Replace grid_fs storage with file

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -2,7 +2,7 @@
 
 class FileUploader < CarrierWave::Uploader::Base
 
-  storage :grid_fs
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
