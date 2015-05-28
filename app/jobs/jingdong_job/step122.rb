2e088b2590edfda75ea0d2a03ff07aa39e1a1eb5
wendi <ifyouseewendy@gmail.com>
2015-05-28 21:32:11 +0800

    Make step 122 display work

---
+++ app/jobs/jingdong_job/step122.rb
@@ -0,0 +1,14 @@
+module JingdongJob
+  class Step122
+    # 检验文件内容与编码
+    def run(step, args)
+      {
+        status: :failed,
+        message: 'sucked'
+      }
+      {
+        status: :succeed,
+      }
+    end
+  end
+end
