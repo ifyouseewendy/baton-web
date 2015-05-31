b3562216530f6eba2c3b0b3139e5492d08d7f645
wendi <ifyouseewendy@gmail.com>
2015-05-31 12:45:15 +0800

    Make step 151 work

---
+++ app/jobs/jingdong_job/step151.rb
@@ -0,0 +1,35 @@
+module JingdongJob
+  class Step151
+    # 填写《产品销售计划表》，并上传、校验
+    def run(step, args)
+      begin
+        source      = Pathname(args[:file].tempfile)
+        check_ext_type!(source)
+
+        target_name = ["guangjiaosuo_产品销售计划表_#{Date.today.to_s.gsub('-','')}", source.extname].join # Auto correct file, or it should be args[:file].original_filename
+        target_path = Pathname(source.dirname).join(target_name)
+        FileUtils.mv(source, target_path)
+
+        step.add_file(target_path, step.recipe, override: true)
+
+        {
+          status: :succeed,
+          type: :file_list,
+          stat: step.file_names.zip(step.file_urls)
+        }
+      rescue => e
+        {
+          status: :failed,
+          message: e.message
+        }
+      end
+    end
+
+    private
+
+      def check_ext_type!(pa)
+        raise "文件类型不是 csv" unless '.csv' == pa.extname.to_s
+      end
+
+  end
+end
