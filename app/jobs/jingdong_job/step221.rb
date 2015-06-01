e002e085bf3ddbcf7cc08c7556b9a46e0e20c986
wendi <ifyouseewendy@gmail.com>
2015-06-01 16:35:15 +0800

    Add step 221

---
+++ app/jobs/jingdong_job/step221.rb
@@ -0,0 +1,27 @@
+module JingdongJob
+  class Step221
+    # 上传《产品销售表》
+    def run(step, args)
+      begin
+        source      = Pathname(args[:file].tempfile)
+        target_name = ["guangjiaosuo_产品销售表_#{Date.today.to_s.gsub('-','')}", source.extname].join # Auto correct file, or it should be args[:file].original_filename
+        target_path = Pathname(source.dirname).join(target_name)
+        FileUtils.mv(source, target_path)
+
+        step.add_file(target_path, step.recipe, override: true)
+
+        {
+          status: :succeed,
+          type: :file_list,
+          stat: step.file_names.zip(step.file_urls)
+        }
+      rescue => e
+        {
+          status: :failed,
+          message: e.message
+        }
+      end
+    end
+
+  end
+end
