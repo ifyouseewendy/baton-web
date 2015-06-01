656f723b1a44eb5ed06dfb5ed981b723c9598a99
wendi <ifyouseewendy@gmail.com>
2015-06-01 21:16:03 +0800

    Add step 212

---
+++ app/jobs/jingdong_job/step212.rb
@@ -0,0 +1,58 @@
+module JingdongJob
+  class Step212
+    # 检验
+    def run(step, args)
+      begin
+        @file = step.task.files.first.try(:file)
+        @pa = Pathname.new @file.try(:current_path)
+
+        total_rows, total_amount, total_fee, rows, amount, fee = [0]*6
+
+        product_summary = Hash.new(0)
+
+        File.open(@pa, 'r:GBK') do |rf|
+          rf.each_with_index do |line, i|
+            next if i == 1
+            next if line.empty?
+
+            columns = line.split("|")
+
+            if i == 0
+              total_rows    = columns[1].split(":").last.to_i
+              total_amount  = columns[2].split(":").last.to_i
+              total_fee     = columns[3].split(":").last.to_i
+            else
+              rows    += 1
+              amount  += columns[7].to_i
+              fee     += columns[10].to_i
+
+              code    = columns[1][-6..-1]
+              product_summary[code] += columns[7].to_i
+            end
+          end
+        end
+
+        check_equality!("总笔数",      total_rows,   rows)
+        check_equality!("总金额",      total_amount, amount)
+        check_equality!("手续费总额",  total_fee,    fee)
+        product_summary.sort_by{|k,v| k}.map{|k,v| puts "#{k}: #{v}"}
+
+        {
+          status: :succeed
+        }
+      rescue => e
+        {
+          status: :failed,
+          message: e.message
+        }
+      end
+    end
+
+    private
+
+      def check_equality!(name, a, b)
+        raise "--> <#{name}> Don't Match: #{a} - #{b}" unless a == b
+      end
+
+  end
+end
