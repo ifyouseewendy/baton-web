df0618580768479fb512f51de8de6a36a1686e81
wendi <ifyouseewendy@gmail.com>
2015-06-03 21:35:57 +0800

    Add UserDetail#to_s

--- app/models/user_detail.rb
+++ app/models/user_detail.rb
@@ -8,4 +8,9 @@ class UserDetail
   field :user_id_card,  type: String
   field :amount,        type: Integer # Xiaojin uses fen as the basic unit, that save 10000 for 100.00 yuan.
 
+  private
+
+    def to_s
+      [product_code, user_name, user_id_card, amount].join(' - ')
+    end
 end
