ccb2dbc4351f8d0645e7feb298d93956d645ad1c
wendi <ifyouseewendy@gmail.com>
2015-06-09 13:53:59 +0800

    Remove xiaojin deprecated thors

--- lib/tasks/xiaojin.rb
+++ lib/tasks/xiaojin.rb
@@ -3,66 +3,6 @@ require 'csv'
 require File.expand_path('../../models/rate_calculator', __FILE__)
 
 class KaitongCli < Thor
-  MAX_CODE_CAPACITY = 200
-
-  desc 'daily_order_check', '处理天级客户销售明细'
-  long_desc <<-LONGDESC
-    Examples:
-
-      ruby lib/tasks/xiaojin.rb daily_order_check --platform=xiaojin --project=alpha
-        --from=/Users/wendi/Workspace/kaitong/ftp-monitor/test/jobs/resources/xiaojin/upload/20150320/xiaojin_天级客户明细销售表_20150320.csv
-  LONGDESC
-  option :from,       required: true
-  option :platform,   required: true
-  option :project,    required: true
-  def daily_order_check
-    load_rails
-
-    DailyOrder.new(options[:platform], options[:project]).check(options[:from])
-  end
-
-  desc 'convert', '将资产单元编号映射成产品代码'
-  long_desc <<-LONGDESC
-    Examples:
-
-      ruby lib/tasks/xiaojin.rb convert --code=K00001 --column=2
-        --from=/Users/wendi/Workspace/kaitong/ftp-monitor/test/tasks/resources/xiaojin/xiaojin_客户明细销售表_20150423.csv
-  LONGDESC
-  option :from,   required: true
-  option :column, type: :numeric, required: true
-  option :code,   required: true
-  def convert
-    raise "Invalid <from> file position: #{options[:from]}" unless File.exist?(options[:from])
-
-    load_rails
-
-    column = options[:column]
-    out_filename = File.basename(options[:from], '.csv')
-    output = File.join(Rails.root, 'tmp', "#{out_filename}.out.csv")
-    code = ProductCode.new(options[:code])
-    File.open(output, 'w') do |of|
-      pos = 0
-      CSV.foreach(options[:from]) do |row|
-        pos += 1
-
-        next if row[0].blank?
-
-        before_code = column > 0 ? row[0..(column-1)] : []
-        after_code  = row[(column+1)..-1]
-        of.puts (before_code + [code.to_s] + after_code).join(',')
-        record_relation(row[column], code)
-
-        if pos == MAX_CODE_CAPACITY
-          pos = 0
-          code.next!
-        end
-      end
-    end
-
-    puts ">> Generate file: #{output}"
-    puts "    Code range: #{options[:code]} - #{code}"
-  end
-
   desc 'generate_gjs_details', "生成广交所需要的《客户明细销售表》"
   long_desc <<-LONGDESC
     Parameters:
@@ -306,12 +246,6 @@ class KaitongCli < Thor
   end
 
   private
-
-    def record_relation(stat, code)
-      prd = Product.find_or_create_by(code: code.to_s)
-      prd.property_serial << stat
-      prd.save!
-    end
 
     def convert_file_encoding!(file, from="UTF-8", to="GBK")
       content = File.read(file, encoding:from)
