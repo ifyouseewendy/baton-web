a5ed0c3b8e91003d1ef9aa4f712ae460791a4459
wendi <ifyouseewendy@gmail.com>
2015-06-09 13:38:36 +0800

    Fix mina unicorn:stop

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,7 +90,7 @@ namespace :unicorn do
   task :stop do
     queue 'echo "-----> Stop Unicorn"'
     queue! %{
-      test -s "#{unicorn_pid}" && kill -QUIT `cat "#{unicorn_pid}"` && echo "Stop Ok" && exit 0
+      test -s "#{unicorn_pid}" && kill -QUIT `cat "#{unicorn_pid}"` && echo "Stop Ok"
       echo >&2 "Not running"
     }
   end
